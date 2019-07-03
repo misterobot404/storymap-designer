64e638a350e580f8f9c7077791344bd1ab311a3c
misterobot <misterobot404@gmail.com>
2019-07-03 16:42:06 +1000

    0.6

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,30 @@
 
 require('./bootstrap');
 window.Vue = require('vue');
+Vue.component('constructor', require('./components/Constructor').default);
 
-//import example from './components/ExampleComponent.vue'
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//********************
+import { Icon }  from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+//**********************
+
 
 new Vue({ el: '#app' });
 
 /*
+import constructor from './components/Constructor.vue'
+
 new Vue({
-    render: h => h(ExampleComponent),
-}).$mount('#app')
+    render: h => h(constructor),
+}).$mount('#app');
 */
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,48 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{asset('favicon.png')}}">
     <title>Map Helper</title>
+    <style>
+        #app {
+            font-family: 'Avenir', Helvetica, Arial, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-align: center;
+            margin: 30px 10%;
+            min-width: 720px;
+        }
+        .mainMenu {
+            vertical-align: middle;
+            margin: auto;
+            padding-left: 0;
+        }
+        .defaultScene {
+            justify-content: center;
+            display: flex;
+            list-style: none;
+            cursor: pointer;
+            height: 2em;
+            transition: color 0.4s ease;
+            transition: font-size 0.4s;
+            color: #000;
+            font-size: 1.5em;
+        }
+        .sceneActive {
+            color: #41B883;
+            font-size: 1.8em;
+        }
+    </style>
 </head>
 <body>
-@section('content')
-    <example-component></example-component>
-@endsection
 
 <div id="app">
-    @yield('content')
+    <ul class="mainMenu">
+        <li class="sceneActive defaultScene">
+            master
+        </li>
+    </ul>
+    <constructor></constructor>
 </div>
+
 <!-- Scripts -->
 <script type="text/javascript" src='{{ asset('/js/app.js') }}'></script>
 
