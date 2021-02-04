75808a49c83d8eaa4ba25cde9ac21606c5b84573
misterobot404 <misterobot404@gmail.com>
2021-02-04 11:38:30 +1000

    1.0.3 yandex metrics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,19 @@
 <div id="app"></div>
 <!-- JS bundle -->
 <script type="text/javascript" src='{{ asset('js/main.js') }}'></script>
+<!-- Yandex.Metrika -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(71874067, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
 </body>
 
 </html>
