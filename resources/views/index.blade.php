ac33bc7e8ce32aee4e9916648160042c3e8af0f9
misterobot404 <misterobot404@gmail.com>
2021-02-05 12:05:45 +1000

    1.0.3 yandex metrics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,13 +22,14 @@
             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
             ym(71940610, "init", {
                 clickmap:false,
                 trackLinks:false,
                 accurateTrackBounce:false,
                 webvisor:true
             });
-        }, 3000)
+        }, 2000)
     });
 </script>
 </body>
