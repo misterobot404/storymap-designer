2b8e043a14bfe0bb817a1007ed3f098c719f95dd
misterobot404 <misterobot404@gmail.com>
2021-02-05 12:42:12 +1000

    1.0.3 yandex metrics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,17 +19,21 @@
 <script type="text/javascript">
     window.addEventListener('load', () => {
         setTimeout(() => {
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
             ym(71940610, "init", {
-                clickmap:false,
-                trackLinks:false,
-                accurateTrackBounce:false,
-                webvisor:true
+                clickmap: false,
+                trackLinks: false,
+                accurateTrackBounce: false,
+                webvisor: true
             });
-        }, 2000)
+        }, 1000)
     });
 </script>
 </body>
